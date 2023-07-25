3682d9e431ef091fcff97239252236ec129f8d46
Gushiken <gusiken0604@gmail.com>
2023-07-26 05:45:02 +0900

    private、アクセス範囲と関数名を意識する

--- Kadai1/ViewController.swift
+++ Kadai1/ViewController.swift
@@ -8,26 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak private var valueTextField1: UITextField!
-    @IBOutlet weak var valueTextField2: UITextField!
-    @IBOutlet weak var valueTextField3: UITextField!
-    @IBOutlet weak var valueTextField4: UITextField!
-    @IBOutlet weak var valueTextField5: UITextField!
-    @IBOutlet weak var result: UILabel!
-    
-    @IBAction func sum_button(_ sender: Any) {
-        
+    @IBOutlet private weak var valueTextField1: UITextField!
+    @IBOutlet private weak var valueTextField2: UITextField!
+    @IBOutlet private weak var valueTextField3: UITextField!
+    @IBOutlet private weak var valueTextField4: UITextField!
+    @IBOutlet private weak var valueTextField5: UITextField!
+    @IBOutlet private weak var result: UILabel!
+    @IBAction private func sumButton(_ sender: Any) {
         let value1 = Int(valueTextField1.text ?? "") ?? 0
         let value2 = Int(valueTextField2.text ?? "") ?? 0
         let value3 = Int(valueTextField3.text ?? "") ?? 0
         let value4 = Int(valueTextField4.text ?? "") ?? 0
         let value5 = Int(valueTextField5.text ?? "") ?? 0
-        
         let total = value1 + value2 + value3 + value4 + value5
         result.text = "\(total)"
-        
     }
-    
 }
-
